2a072c4dc729ce86ba635764e4889624105b63a8
thuptit <thunv.B18CN655@stu.ptit.edu.vn>
2021-11-09 21:30:23 +0700

    fix node-sass

--- src/Layout/components/Footer/Footer.jsx
+++ src/Layout/components/Footer/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function Footer() {
-  return <div>Hello from footer</div>;
+  return (
+    <footer>
+      <div className="container-fluid bg-secondary">
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <div className="row">
+              <div className="col-md-4">
+                hello
+              </div>
+              <div className="col-md-4">
+                hello
+              </div>
+              <div className="col-md-4">
+                hello
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
