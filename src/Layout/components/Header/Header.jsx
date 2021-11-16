f9269433884470722788d9422aa9ee412ffbdb09
Hoang Huy <hoangvh@jitsinnovationlabs.com>
2021-11-16 10:42:08 +0700

    update link login

--- src/Layout/components/Header/Header.jsx
+++ src/Layout/components/Header/Header.jsx
@@ -1,18 +1,35 @@
-import React ,{useEffect} from "react";
-import { Nav, Container, Row, Col, Dropdown, InputGroup, FormControl, Button } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import {
+  Nav,
+  Container,
+  Row,
+  Col,
+  Dropdown,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import "./Header.scss";
-import logo from './scart-mid.png';
-import catalogLogo from './catalog.png';
-import { useHistory } from 'react-router-dom';
+import logo from "./scart-mid.png";
+import catalogLogo from "./catalog.png";
+import { useHistory } from "react-router-dom";
 function Header(props) {
   const history = useHistory();
   return (
     <Container fluid>
-      <Row className="contact-line py-1" >
+      <Row className="contact-line py-1">
         <ul class="nav nav-pills d-flex align-items-center justify-content-between">
-          <div className="d-flex flex-row" >
-            <li><a href="tel:0123456789"><i class="fa fa-phone"></i> 0123456789 </a></li>
-            <li><a href="#"><i class="fa fa-envelope"></i> ndhieuit209@gmail.com </a></li>
+          <div className="d-flex flex-row">
+            <li>
+              <a href="tel:0123456789">
+                <i class="fa fa-phone"></i> 0123456789{" "}
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i class="fa fa-envelope"></i> ndhieuit209@gmail.com{" "}
+              </a>
+            </li>
           </div>
           <li>
             <Dropdown>
@@ -21,34 +38,47 @@ function Header(props) {
                 <span>User</span>
               </Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Login</Dropdown.Item>
+                <Dropdown.Item
+                  onClick={() => {
+                    history.push("/login");
+                  }}
+                >
+                  Login
+                </Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </li>
         </ul>
       </Row>
       <Row className="header d-flex align-items-center py-3 ">
-        <Col className="d-flex justify-content-center" md={4} sm={12} xl={4} >
+        <Col className="d-flex justify-content-center" md={4} sm={12} xl={4}>
           <img className="logo" src={logo} />
         </Col>
-        <Col className="d-flex flex-row" md={8} sm={12} xl={8} >
+        <Col className="d-flex flex-row" md={8} sm={12} xl={8}>
           <InputGroup className="w-50">
             <FormControl
               placeholder="Your keyword..."
               aria-label="Recipient's username"
               aria-describedby="basic-addon2"
             />
-            <InputGroup.Text id="basic-addon2"><i class="fas fa-search"></i></InputGroup.Text>
+            <InputGroup.Text id="basic-addon2">
+              <i class="fas fa-search"></i>
+            </InputGroup.Text>
           </InputGroup>
-          <div class="ms-5" >
+          <div class="ms-5">
             <Button variant="outline-primary">
               <i class="fas fa-heart me-2"></i>
-              <span >Wish list</span></Button>
-            <Button onClick={()=>{
-              history.push('/Cart')
-            }} variant="outline-primary ms-3">
+              <span>Wish list</span>
+            </Button>
+            <Button
+              onClick={() => {
+                history.push("/Cart");
+              }}
+              variant="outline-primary ms-3"
+            >
               <i class="fa fa-shopping-cart me-2"></i>
-              <span >View cart</span></Button>
+              <span>View cart</span>
+            </Button>
           </div>
         </Col>
       </Row>
@@ -62,13 +92,15 @@ function Header(props) {
         <Col className="d-flex justify-content-end " sm={8} md={8}>
           <Nav
             className="navbar"
-          // activeKey="/home"
+            // activeKey="/home"
           >
             <Nav.Item>
               <Nav.Link eventKey="link-1">Home</Nav.Link>
             </Nav.Item>
-            <Nav.Item className="dropdown" >
-              <Nav.Link eventKey="link-2">Shop<i class="fas fa-caret-down ms-1"></i></Nav.Link>
+            <Nav.Item className="dropdown">
+              <Nav.Link eventKey="link-2">
+                Shop<i class="fas fa-caret-down ms-1"></i>
+              </Nav.Link>
               <div class="dropdown-content">
                 <a href="#">Link 1</a>
                 <a href="#">Link 2</a>
@@ -79,7 +111,7 @@ function Header(props) {
               <Nav.Link eventKey="link-2">About us</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link-3" >Contact us</Nav.Link>
+              <Nav.Link eventKey="link-3">Contact us</Nav.Link>
             </Nav.Item>
           </Nav>
         </Col>
